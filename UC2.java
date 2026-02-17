804822c6c7999df1caf25e49164126915fdd340d
p145678-max <pp5664@srmist.edu.in>
2026-02-17 22:54:30 +0530

    chore: code UC2 to print OOPS banner to console

---
+++ UC2.java
@@ -0,0 +1,15 @@
+/*
+* OOPBannerApp UC2: Print OOP in a banner in console
+*/
+
+public class OOPBannerApp {
+	public static void main(String[] args) {
+		System.out.println("    ***    " + "    ***    " + " *******  " + "   *****  ");
+		System.out.println("  **   **  " + "  **   **  " + " **    ** " + " **    ** ");
+		System.out.println(" **     ** " + " **     ** " + " **    ** " + " **       ");
+		System.out.println(" **     ** " + " **     ** " + " *******  " + "  *****   ");
+		System.out.println(" **     ** " + " **     ** " + " **       " + "       ** ");
+		System.out.println("  **   **  " + "  **   **  " + " **       " + " **    ** ");
+		System.out.println("    ***    " + "    ***    " + " **       " + "   *****  ");
+	}
+}
